4cc1715030f722ea0d131574aa6f0153435bdff7
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-18 12:24:24 +0100

    creating default state

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class CurrencyConverter extends Component {
+
+    state = {
+        currencies: ["USD", "EUR"],
+        base: "USD",
+        amount: "",
+        convertTo: "EUR",
+        result: ""
+    };
+
     render(){
         return(
             <div>
